f3e83d9231597bc0279dbfb16efe54c6563472ad
Raymond Lee <87112683+LaymondRee@users.noreply.github.com>
2022-07-22 23:25:24 -0700

    Create sealion.h

---
+++ sealion.h
@@ -0,0 +1,20 @@
+/**************************************************
+ * Filename: sealion.h
+ * Author: Raymond Lee
+ * Date: 5-9-2021
+ * Description: Contains all members of Sealion class
+ **************************************************/
+
+#ifndef SEALION_H
+#define SEALION_H
+
+#include "animals.h"
+
+class Sealion: public Animals{
+	private:
+
+	public:
+		Sealion();
+};
+
+#endif
