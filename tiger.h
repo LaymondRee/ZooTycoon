d5f1c8dac4181a5deb71fa8a13ec355fab64fddc
Raymond Lee <87112683+LaymondRee@users.noreply.github.com>
2022-07-22 23:26:04 -0700

    Create tiger.h

---
+++ tiger.h
@@ -0,0 +1,20 @@
+/**************************************************
+ * Filename: tiger.h
+ * Author: Raymond Lee
+ * Date: 5-9-2021
+ * Description: Contains all members of Tiger class
+ **************************************************/
+
+#ifndef TIGER_H
+#define TIGER_H
+
+#include "animals.h"
+
+class Tiger: public Animals{
+	private:
+
+	public:
+		Tiger();
+};
+
+#endif
