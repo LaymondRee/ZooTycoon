2c120c7f37584bd99f2462891e48ab0b76c8bc81
Raymond Lee <87112683+LaymondRee@users.noreply.github.com>
2022-07-22 23:27:09 -0700

    Create blackbear.h

---
+++ blackbear.h
@@ -0,0 +1,20 @@
+/**************************************************
+ * Filename: blackbear.h
+ * Author: Raymond Lee
+ * Date: 5-9-2021
+ * Description: Contains all members of Blackbear class
+ **************************************************/
+
+#ifndef BLACKBEAR_H
+#define BLACKBEAR_H
+
+#include "animals.h"
+
+class Blackbear: public Animals{
+	private:
+
+	public:
+		Blackbear();
+};
+
+#endif
